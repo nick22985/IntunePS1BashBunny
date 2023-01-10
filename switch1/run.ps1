1f450e0890ab6d2a14bba8948faaee33a841083c
nick22985 <nick22985@hotmail.com>
2023-01-10 22:28:57 +1000

    feat: auto update testing

--- switch1/run.ps1
+++ switch1/run.ps1
@@ -1,8 +1,54 @@
 ﻿$VolumeName = "bashbunny"
 $computerSystem = Get-CimInstance CIM_ComputerSystem
 $backupDrive = $null
+get-wmiobject win32_logicaldisk | % {
+    if ($_.VolumeName -eq $VolumeName) {
+        $backupDrive = $_.DeviceID
+    }
+}
 
-$envVars = Import-Csv -Path ".env" -Delimiter ","
+
+# Define the repository owner and name
+$owner = "nick22985"
+$repo = "IntunePS1BashBunny"
+
+# Define the current version of the script
+$currentVersion = "0"
+
+# Get the latest release from GitHub
+$release = (Invoke-RestMethod -Uri "https://api.github.com/repos/$owner/$repo/releases/latest").tag_name
+Write-Host $release
+Write-Host $backupDrive
+# Compare the latest release with the current version
+if ($release -gt $currentVersion) {
+    # Download the new files
+    Invoke-WebRequest -Uri "https://github.com/$owner/$repo/archive/refs/tags/$release.zip" -OutFile "update.zip"
+    # Unzip the downloaded file
+    Expand-Archive -Path "update.zip" -DestinationPath "update"
+
+    # Copy env file to update
+    Copy-Item -Path "../.env" -Destination "update/$repo-$release/.env" -Force
+
+    # Define the location of the old script
+    $oldScriptLocation = $backupDrive + "/payloads/"
+
+    # Move the new files to the location of the old script, overwriting existing files
+    Move-Item -Path "update\*" -Destination $oldScriptLocation -Force
+
+    # Delete the old files
+    Remove-Item -Recurse -Force $oldScriptLocation\update
+
+    # Run the update script
+    & "$oldScriptLocation\update.ps1"
+
+} else {
+    # No update available
+    Write-Host "No update available."
+}
+
+
+
+$envVars = Import-Csv -Path "../.env" -Delimiter ","
 $localEnvVars = @{}
 foreach ($envVar in $envVars) {
     if ($envVar.Value -ne $null -and $envVar.Value -ne "" -and $envVar.Name -ne $null -and $envVar.Name -ne "") {
@@ -51,12 +97,6 @@ if($localEnvVars.Group -eq $null) {
 $UserAccount = Read-host -Prompt "User Account"
 
 
-get-wmiobject win32_logicaldisk | % {
-    if ($_.VolumeName -eq $VolumeName) {
-        $backupDrive = $_.DeviceID
-    }
-}
-
 #See if a loot folder exist in usb. If not create one
 $TARGETDIR = $backupDrive + "\loot"
 if(!(Test-Path -Path $TARGETDIR )){
@@ -78,12 +118,7 @@ $TARGETDIR = $MyInvocation.MyCommand.Path
 $TARGETDIR = $TARGETDIR -replace ".......$"
 cd $TARGETDIR
 
-
 $email = $UserAccount + $localEnvVars.domain
-
-
-
-
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
 Install-Script -Name Get-WindowsAutopilotInfo -Force -Confirm:$False
